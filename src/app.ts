8ee32b6a2459f34242c7758119ed305823321364
Busra2456 <you@example.com>
2026-06-24 13:47:01 +0600

    Registering First User With Prisma And Register API

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,10 @@ import express,{ Application, Request, Response } from "express";
 import config from "./config";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import httpStatus from "http-status";
 import { prisma } from "./lib/prisma";
+import bcrypt from "bcryptjs";
+
 
 const app : Application = express();
 
@@ -17,6 +20,59 @@ app.use(cookieParser())
 app.get("/", async(req :Request, res : Response) =>{
     
       res.send("Hello world")
+});
+
+app.post("/api/users/register", async (req:Request,res :Response)=>{
+      const {name , email, password, profilePhoto} = req.body;
+      const isUserExist = await prisma.user.findUnique({
+            where : {email}
+
+      })
+      if(isUserExist){
+            throw new Error("user with this email already exists");
+      }
+      const hashedPassword = await bcrypt.hash(password,Number(config.bcrypt_salt_rounds))
+    
+      const createUser = await prisma.user.create({
+            data : {
+                  name,
+                  email,
+                  password : hashedPassword,
+            }
+
+      })
+
+      await prisma.profile.create({
+            data : {
+                  userId : createUser.id,
+                  profilePhoto
+            }
+
+      })
+
+      const user = await prisma.user.findUnique({
+            where : {
+                  id : createUser.id,
+                  email : createUser.email || email
+            },
+            omit : {
+                  password : true
+                  
+
+            },
+            include : {
+                  profile : true
+            }
+      })
+
+      res.status(httpStatus.CREATED).json({
+            success : true,
+            statuscode : httpStatus.CREATED,
+            message : "user registered successfully",
+            data : {
+                  user
+            }
+      })
 })
 
 export default app;
