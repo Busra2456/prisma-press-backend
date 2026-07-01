856e4439e9719350a58daabc7ef06234b214843b
Busra2456 <you@example.com>
2026-07-01 15:03:21 +0600

    Dynamic Search, Filtering, Pagination, Sorting With API Request Query and Not Found Route And Prisma Errors With Global Error Handler

--- src/modules/post/post.interface.ts
+++ src/modules/post/post.interface.ts
@@ -1,4 +1,5 @@
 import { PostStatus } from "../../../generated/prisma/enums";
+import { PostWhereInput } from "../../../generated/prisma/models";
 export interface ICreatePostPayload {
     title : string;
     content : string;
@@ -16,3 +17,13 @@ export interface IUpdatePostPayload{
     status?: PostStatus
     tags?: string[]
 }
+
+export interface IPostQuery extends PostWhereInput {
+   
+
+    searchTerm?: string
+    page?: string
+    limit?: string
+    sortOrder?: string
+    sortBy?: string
+} 
